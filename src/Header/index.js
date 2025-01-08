f03c6034f49aad7ddb5cbf3fbc848a185ceee2a1
vitoria.sousa <maria.v.cosmo@gmail.com>
2025-01-08 11:16:39 -0300

    aprendendo sobre memorização de componentes

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,11 +1,25 @@
 import './header.css';
-let renderCount = 0;
 
-export function Header() {
-  renderCount++;
+import { memo } from 'react';
+
+// export function Header({ name }) {
+//   console.log('Componente header renderizou!')
   
-  return (
-   <h3 className='header'>renderizou {renderCount}</h3>
-  )
-}
+//   return (
+//    <h3 className='header'>Bem-vindo(a) {name}</h3>
+//   )
+// }
 
+// // memorizar o nosso componente header
+// // criamos uma nova instância chamada memorized header, em que ela está memorizando o componente header
+// export const MemorizedHeader = memo(Header)
+
+// froma mais enxuta de criar o memo do componente
+
+export const Header = memo(({ name }) => {
+  console.log('Componente header renderizou')
+
+  return (
+    <h3 className='header'>Bem-vindo {name}</h3>
+  )
+})
